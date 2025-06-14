57bf368141965faa67412910d45e1432cc92a5b8
keydoesthings <keydoesthings@gmail.com>
2025-06-14 06:17:44 -0700

    create item system and inventory

---
+++ Source/RPG_Game/Public/InventoryComponent.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InventoryComponent.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class RPG_GAME_API UInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+	
+};
--- Source/RPG_Game/Public/ItemDataAsset.h
+++ Source/RPG_Game/Public/ItemDataAsset.h
@@ -6,12 +6,25 @@
 #include "Engine/DataAsset.h"
 #include "ItemDataAsset.generated.h"
 
-/**
- * 
- */
-UCLASS()
+UCLASS(BlueprintType, Blueprintable)
 class RPG_GAME_API UItemDataAsset : public UDataAsset
 {
 	GENERATED_BODY()
+
+public:
 	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FString Name;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	float HealthEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	float EnergyEffect;
+	
+	UItemDataAsset()
+		: Name(TEXT("Default Item"))
+		, HealthEffect(0.f)
+		, EnergyEffect(0.f)
+	{}
 };
