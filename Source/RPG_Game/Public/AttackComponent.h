f84d9c63b181cce3b5cd24720c44eb0639204695
keydoesthings <keydoesthings@gmail.com>
2025-06-16 16:00:25 -0700

    idk honestly

--- Source/RPG_Game/Public/AttackComponent.h
+++ Source/RPG_Game/Public/AttackComponent.h
@@ -3,15 +3,34 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/SceneComponent.h"
+#include "Components/ActorComponent.h"
 #include "AttackComponent.generated.h"
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class RPG_GAME_API UAttackComponent : public USceneComponent
+class RPG_GAME_API UAttackComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	FVector BoxOffset = FVector(0.f, 50.f, 0.f);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	FVector BoxHalfSize = FVector(50.f, 50.f, 50.f);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	bool bDebug = false;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	FColor DebugHitColor = FColor::Red;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	FColor DebugMissColor = FColor::Green;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	float DebugDuration = 2.f;
+	
 	UFUNCTION(BlueprintCallable, Category = "Attack")
-	void Attack();
+	bool Attack(FHitResult& OutHit);
 };
