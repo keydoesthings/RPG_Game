1949d6c2f0d9117c5389e606906d6c2b78dfb6bb
keydoesthings <keydoesthings@gmail.com>
2025-06-18 06:48:40 -0700

    idk

--- Source/RPG_Game/Private/RPGCharacter.cpp
+++ Source/RPG_Game/Private/RPGCharacter.cpp
@@ -7,23 +7,17 @@
 // Sets default values
 ARPGCharacter::ARPGCharacter()
 {
- 	// Set this character to call Tick() every frame.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// Create components
 	Health = CreateDefaultSubobject<UHealthComponent>(FName("Health"));
 	Inventory = CreateDefaultSubobject<UInventoryComponent>(FName("Inventory"));
 	Attack = CreateDefaultSubobject<UAttackComponent>(FName("Attack"));
 
-	// Setup capsule collision for overlaps
 	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 	GetCapsuleComponent()->SetCollisionObjectType(ECC_Pawn);
 	GetCapsuleComponent()->SetGenerateOverlapEvents(true);
 	GetCapsuleComponent()->SetCollisionResponseToAllChannels(ECR_Block);
-	// Make sure capsule overlaps pickups which are typically WorldDynamic or custom channel
 	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_GameTraceChannel1, ECR_Overlap);
-	// Optional: ignore other pawns to avoid blocking
-	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
 }
 
 // Called when the game starts or when spawned
